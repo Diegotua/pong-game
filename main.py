30d7cba5721be3837374149d1a17081b628a1856
Diego Jose Tua Guevara <tuagdiego@gmail.com>
2022-10-26 18:55:32 -0400

    sound and score

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-
 import pygame
 import random as rd
+from pygame import mixer
 pygame.init()
 rgb=(21, 28, 96 )
 players_colors=(48, 96, 21 )
 ball_color=(21, 96, 91 )
 line_color=(0,0,0)
+mixer.music.load("fondo.mp3")
+mixer.music.play(-1)
+mixer.music.set_volume(0.5)
 screen_width=500
 screen_height=400
-
 size =(screen_width,screen_height)
 
 screen=pygame.display.set_mode(size)
@@ -26,6 +28,23 @@ ball_radius=20
 ball_speed_x=0.1
 ball_speed_y=0.1
 pygame.display.set_caption("pong")
+player_1_score=0
+player_2_score=0
+score_font=pygame.font.Font("font.ttf",20)
+won_font=pygame.font.Font("font.ttf",40)
+player_1_score_x=10
+player_1_score_y=10
+player_2_score_x=screen_width-165
+player_2_score_y=10
+won_x=130
+won_y=130
+def show_score_1(x,y):
+    score1=score_font.render("player one : " +str(player_1_score),True,(21, 96, 23) )
+    screen.blit(score1,(x,y))
+def show_score_2(x,y):
+    score2=score_font.render("player two: " +str(player_2_score),True,(21, 96, 23) )
+    screen.blit(score2,(x,y))
+
 icon=pygame.image.load("ping-pong.png")
 pygame.display.set_icon(icon)
 
@@ -76,11 +95,17 @@ while running:
         ball_speed_y *= -1
 
     if ball_x>screen_width:
+        lose_sound=mixer.Sound("lost.wav")
+        lose_sound.play()
+        player_1_score+=10
         ball_x = screen_width/2
         ball_y= screen_height/2
         ball_speed_x*= rd.choice([-1,1])
 
     elif ball_x <0:
+        lose_sound=mixer.Sound("lost.wav")
+        lose_sound.play()
+        player_2_score+=10
         ball_x = screen_width/2
         ball_y= screen_height/2
         ball_speed_x*= rd.choice([-1,1])
@@ -105,6 +130,30 @@ while running:
     pygame.draw.aaline(screen,line_color,(screen_width/2,0),(screen_width/2,screen_height))
     ball=pygame.draw.circle(screen,ball_color,(ball_x,ball_y),ball_radius)
     
+    if ball.colliderect(player_1)or ball.colliderect(player_2):
+        ball_speed_x *= -1
+        ball_sonido=mixer.Sound("choque.wav")
+        ball_sonido.play()
+    if player_1_score ==50:
+        ball_y=2000
+        ball_speed_x=0
+        ball_speed_y=0
+        player_1_y_speed=0
+        player_2_y_speed=0
+        won_text=won_font.render("player 1 won", True, (0,0,0))
+        screen.blit(won_text,(won_x,won_y))
 
+    elif player_2_score==50:  
+        ball_y=2000
+        ball_speed_x=0
+        ball_speed_y=0
+        player_1_y_speed=0
+        player_2_y_speed=0
+        won_text=won_font.render("player 2 won", True, (0,0,0))
+        screen.blit(won_text,(won_x,won_y))
+
+    show_score_1(player_1_score_x,player_1_score_y)
+    show_score_2(player_2_score_x,player_2_score_y)    
     
+
     pygame.display.flip()                                                                                     
